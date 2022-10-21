5682915211dec147c83379969b86392ec19b07b6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-21 23:15:39 +0600

    update next.js

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import { Fragment } from "react";
+
 //our-domain.com/news
 
 function NewsPage() {
   return (
     <Fragment>
       <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJs Is a Great Framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
     </Fragment>
   );
 }
