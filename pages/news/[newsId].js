4acd35e5b213cf4f83613e840ba9c659599da650
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-21 23:04:08 +0600

    update next.js

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,11 @@
+import {useRouter} from 'next/router';
 //our-domain.com/news/something-important
 
 function DetailPage (){
+ const router = useRouter();
+
+const newsId = router.query.newsId;
+
     return <h1>The  Detail Page</h1>
   }
   export default DetailPage;
